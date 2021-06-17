2cedc24061a2988ec8a4cada0920fbd4afe43aa7
qijiacheng333 <qijiacheng333@163.com>
2021-06-18 00:22:26 +0800

    更新

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-function fun1(){
-    alert('hello git')
+function init(){
+    var num=document.getElementById('num');
+    num.value=0;
+    num.disabled='disabled'; //禁用，防止误输入
+    //实现输入显示
+    var oButton=document.getElementsByTagName("input");
+    for (var i=0;i<oButton.length;i++){
+        oButton[i].onclick=function(){
+            if (!isNaN(this.value)){ //判断单击是否为数字
+                if (isNaN(num.value)){                  //判断文本框是否为数字
+                    num.value=this.value
+                }else{
+                num.value=(num.value+this.value)*1; //this代表oButton[i]，*1隐式转换
+                }
+            }else{
+                switch(this.value){
+                case '+':
+                    break;
+                
+                }
+            }
+        }
+    }
 }
